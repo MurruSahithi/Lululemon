7490496a9a5d0cb0c1370e9d44dbfdb71eae3721
MurruSahithi <murrusahithi@gmail.com>
2024-05-13 19:07:59 -0400

    my 4th commit

--- src/test/java/testRunner/testRun.java
+++ src/test/java/testRunner/testRun.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = ".//Features/searchFeatures.feature", glue = {"stepDefinitions"}, dryRun = false, plugin = {"pretty","html:test-output"})
+@CucumberOptions(features = ".//Features/searchFeatures.feature", glue = {"stepDefinitions"}, dryRun = false, plugin = {"pretty","json:target/MyReports/report.json","junit:target/MyReports/report.xml","html:target/MyReports/report.html"},publish = true)
 public class testRun {
 
 }
